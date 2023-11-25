a354e1abd16de2b45006fa18fc2a92034e37abd3
Iván Berlanga <ivan@MacBook-Pro-de-Ivan.local>
2023-11-25 05:17:10 +0100

    new

--- next.config.js
+++ next.config.js
@@ -1,17 +1,31 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {}
 
-module.exports = nextConfig
+const isGithubActions = process.env.GITHUB_ACTIONS || false
+
+let assetPrefix = ''
+let basePath = '/'
+
+if (isGithubActions) {
+  const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '')
+
+  assetPrefix = `/${repo}/`
+  basePath = `/${repo}`
+}
+
 module.exports = {
+    eslint: {
+        // Warning: This allows production builds to successfully complete even if
+        // your project has ESLint errors.
+        ignoreDuringBuilds: true,
+    },
+    assetPrefix: assetPrefix,
+    basePath: basePath,
     images: {
         unoptimized: true
     },
     trailingSlash: true,
-    eslint: {
-      // Warning: This allows production builds to successfully complete even if
-      // your project has ESLint errors.
-      ignoreDuringBuilds: true,
-    },
+
     output: 'export'
   }
-
+ 
